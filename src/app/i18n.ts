20136a05b05fc21deded539909a6b6d480a58a95
Kotorkovsciy <kotorkovsciy@yandex.ru>
2024-10-10 10:14:19 +0300

    🔧 chore: update Vite config, i18n setup, and GitHub Actions
    - Refactored Vite config for optimized aliasing and REPO_NAME handling.
    - Integrated i18next with backend path support for dynamic repo names.
    - Updated GitHub Actions to automatically set REPO_NAME and deploy to gh-pages.

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -3,8 +3,19 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-  fallbackLng: "en",
-});
+const repoName = import.meta.env.REPO_NAME
+  ? `/${import.meta.env.REPO_NAME}/`
+  : "/";
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    backend: {
+      loadPath: `${repoName}locales/{{lng}}/translation.json`,
+    },
+  });
 
 export default i18n;
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,39 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      "@/assets": path.resolve(__dirname, "./src/assets/"),
-    },
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom"],
-    exclude: [],
-  },
-  server: {
-    port: 2600,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: "./",
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+        "@/assets": path.resolve(__dirname, "./src/assets/"),
       },
     },
-  },
+    optimizeDeps: {
+      include: ["react", "react-dom"],
+      exclude: [],
+    },
+    server: {
+      port: 2600,
+      open: true,
+    },
+    build: {
+      outDir: "dist",
+      sourcemap: false,
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+    define: {
+      "import.meta.env.REPO_NAME": JSON.stringify(env.REPO_NAME),
+    },
+  };
 });
